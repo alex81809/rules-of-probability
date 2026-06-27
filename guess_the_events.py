25ffb1403bd82bde71a71ec4c49671bccf9b7e94
Alexandra <asjlobaton@gmail.com>
2026-06-27 11:47:50 +0300

    Create guess_the_events.py

---
+++ guess_the_events.py
@@ -0,0 +1,10 @@
+total_balls = 4
+red_balls = 1
+
+prob_red = red_balls / total_balls
+
+# Probability of not picking a red ball
+prob_not_red = 1 - prob_red
+
+print('Probability of getting a red ball: ', prob_red)
+print('Probability of not getting a red ball: '. prob_not_red)
